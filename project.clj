daeabae3c72e1e0d075d27dc603f7aff54b91159
Jorge Colindres <jorge@workframe.com>
2018-04-16 14:25:57 -0400

    Update license info in project.clj and package.json

--- project.clj
+++ project.clj
@@ -1,15 +1,14 @@
 (defproject com.workframe/artemis "0.1.0-SNAPSHOT"
   :description "A minimal ClojureScript GraphQL client"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/workframers/artemis"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [cljs-http "0.1.44"]
                  [alumbra/parser "0.1.7"]
                  [alumbra/errors "0.1.1"]]
-  :plugins [[lein-doo "0.1.8"]
-            [lein-cljsbuild "1.1.7"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :aliases {"test" ["test-jvm"]
             "test-chrome" ["with-profile" "test" "doo" "chrome" "test" "once"]
             "test-jvm" ["with-profile" "test" "doo" "rhino" "test" "once"]
@@ -29,7 +28,8 @@
           :dependencies [[codox-theme-rdash "0.1.2"]]}
    :examples {:source-paths ["examples/common"]
               :dependencies [[re-frame "0.10.5"]]}
-   :test {:dependencies [[org.mozilla/rhino "1.7.7"]
+   :test {:plugins [[lein-doo "0.1.8"]]
+          :dependencies [[org.mozilla/rhino "1.7.7"]
                          [org.clojure/test.check "0.9.0"]
                          [orchestra "2017.11.12-1"]]
           :doo {:paths {:karma "node_modules/.bin/karma"
